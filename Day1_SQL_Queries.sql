fa93458cac9032f40cc1beac00f4e1158f553122
Lourdes María Alonso <alonsolourdesmaria5@gmail.com>
2026-05-03 19:53:03 -0300

    Sorting related records using JOIN and descending criteria
    Implementation of a query that links two tables to organize the entire movie catalog based on their rating. The ORDER BY ... DESC clause is used to prioritize the items with the highest score in the final result.

--- Day1_SQL_Queries.sql
+++ Day1_SQL_Queries.sql
@@ -82,3 +82,8 @@ FROM movies
 JOIN boxoffice
 ON movies.id = boxoffice.movie_id
 WHERE international_sales > domestic_sales;
+SELECT title, rating
+FROM movies
+JOIN boxoffice
+ON movies.id = boxoffice.movie_id
+ORDER BY rating DESC;
